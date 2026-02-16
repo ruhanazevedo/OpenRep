ab3244c71e0b7502b08a460e6d883ad2a97cf8ec
ruhan <ruhanv.azvdo@gmail.com>
2026-02-16 09:06:55 +0000

    chore: rename app to OpenRep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.ruhanazevedo.workoutgenerator"
+        applicationId = "com.ruhanazevedo.openrep"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
