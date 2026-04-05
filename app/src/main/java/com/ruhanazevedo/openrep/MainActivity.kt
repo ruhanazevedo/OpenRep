7d273f1e87f1273ba4bf8c90294e64190f282bf5
ruhan <ruhanv.azvdo@gmail.com>
2026-04-05 16:48:00 +0000

    fix missing getValue import for State delegate in MainActivity

--- app/src/main/java/com/ruhanazevedo/openrep/MainActivity.kt
+++ app/src/main/java/com/ruhanazevedo/openrep/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ruhanazevedo.openrep.data.repository.PreferencesRepository
 import com.ruhanazevedo.openrep.ui.theme.WorkoutGeneratorTheme
